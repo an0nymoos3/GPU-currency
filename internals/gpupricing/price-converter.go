4f3c0c953c29ab00eb684ca99f2183c3007d8bb3
an0nymoos3 <moosely123@gmail.com>
2024-02-26 14:11:00 +0100

    more verbose

--- internals/gpupricing/price-converter.go
+++ internals/gpupricing/price-converter.go
@@ -1,7 +1,10 @@
 package gpupricing
 
+import "fmt"
+
 func ConvertToGpud(price float64, currency string) float64 {
 	if currency != "USD" {
+		fmt.Println("Sending currency conversion request...")
 		price = XeConvertToUsd(price, currency)
 	}
 	return price / float64(GetGPUPrice())
